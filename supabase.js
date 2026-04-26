df461385574a2628ac914aacfe182df81b9ad947
jacobfrogjacob-dotcom <jacob.frog.jacob@gmail.com>
2026-04-26 17:05:19 +0800

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://chjuzpbocdkdiyuuziye.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoanV6cGJvY2RraXl1eml5ZSIsInJvbGUiOiJYW5vbiIsImlhdCI6MTY0ODE4MzIwMCwiZXhwIjoxOTYzNzU5NjAwfQ.kFflCNsyHLFgc8Ee_iWAEw_q5AFG3YC';
+const SUPABASE_KEY = 'sb_publishable_kFflCNsyHLFgc8Ee_iWAEw_q5AFG3YC';
 
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 window.supabaseClient = _supabase;
